f3ea76c2f0d065abfe1f57aba2105ab26a916f47
Semyon Danilov <samvimes@yandex.ru>
2014-11-11 22:41:23 +0300

    added static all method that produce Promise for parameters promises data Also a static resolve method was added (generates Promise for passed data)

--- src/com/danilov/Main.java
+++ src/com/danilov/Main.java
@@ -10,55 +10,84 @@ public class Main {
 
     public static void main(String[] args) {
 
-        final Promise<Integer> integerPromise = new Promise<Integer>();
+//        final Promise<Integer> integerPromise = new Promise<Integer>();
+//
+//        Thread t1 = new Thread() {
+//            @Override
+//            public void run() {
+//                Integer a = 25;
+//                Integer b = 40;
+//                try {
+//                    Thread.sleep(5000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println("THREAD1");
+//                integerPromise.finish(a + b, true);
+//            }
+//        };
+//        final Promise<Double> doublePromise = integerPromise.then(new Promise.Action<Integer, Double>() {
+//            @Override
+//            public Double action(final Integer data) {
+//                return Double.valueOf("" + data) + 0.25d;
+//            }
+//        });
+//        Thread t2 = new Thread() {
+//            @Override
+//            public void run() {
+//                stringPromise = doublePromise.then(new Promise.Action<Double, String>() {
+//                    @Override
+//                    public String action(final Double data) {
+//                        return "" + data;
+//                    }
+//                });
+//                System.out.println("THREAD2");
+//                stringPromise.then(new Promise.Action<String, Void>() {
+//                    @Override
+//                    public Void action(final String data) {
+//                        finalPromise.finish(data, true);
+//                        return null;
+//                    }
+//                });
+//            }
+//        };
+//        t2.start();
+//        t1.start();
+//        finalPromise.then(new Promise.Action<String, Object>() {
+//            @Override
+//            public Object action(final String data) {
+//                System.out.println(data);
+//                return null;
+//            }
+//        });
 
+        final Promise<Integer> p1 = new Promise<Integer>();
         Thread t1 = new Thread() {
             @Override
             public void run() {
-                Integer a = 25;
-                Integer b = 40;
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("THREAD1");
-                integerPromise.finish(a + b, true);
+                p1.finish(21, true);
             }
         };
-        final Promise<Double> doublePromise = integerPromise.then(new Promise.Action<Integer, Double>() {
-            @Override
-            public Double action(final Integer data) {
-                return Double.valueOf("" + data) + 0.25d;
-            }
-        });
+        final Promise<Integer> p2 = new Promise<Integer>();
         Thread t2 = new Thread() {
             @Override
             public void run() {
-                stringPromise = doublePromise.then(new Promise.Action<Double, String>() {
-                    @Override
-                    public String action(final Double data) {
-                        return "" + data;
-                    }
-                });
-                System.out.println("THREAD2");
-                stringPromise.then(new Promise.Action<String, Void>() {
-                    @Override
-                    public Void action(final String data) {
-                        finalPromise.finish(data, true);
-                        return null;
-                    }
-                });
+                p2.finish(21, true);
             }
         };
-        t2.start();
-        t1.start();
-        finalPromise.then(new Promise.Action<String, Object>() {
+
+        Promise.all(p1, p2).then(new Promise.Action<Object[], Object>() {
             @Override
-            public Object action(final String data) {
-                System.out.println(data);
+            public Object action(final Object[] data) {
+                Integer a = (Integer) data[0];
+                Integer b = (Integer) data[1];
+                System.out.println(a + b);
                 return null;
             }
         });
+
+        t1.start();
+        t2.start();
+
     }
 }
--- src/com/danilov/promise/Promise.java
+++ src/com/danilov/promise/Promise.java
@@ -39,6 +39,49 @@ public class Promise<Type> {
         return nPromise;
     }
 
+    public static <X> Promise<X> resolve(final X data) {
+        Promise<X> promise = new Promise<X>();
+        promise.finish(data, true);
+        return promise;
+    }
+
+    public static Promise<Object[]> all(final Promise... promises) {
+        int count = promises.length;
+        final Promise<Object[]> promise = new Promise<Object[]>();
+        final Counter counter = new Counter();
+        counter.count = count;
+        final Object[] dataArray = new Object[promises.length];
+        for (int i = 0; i < count; i++) {
+            final int num = i;
+            promises[i].then(new Action() {
+                @Override
+                public Object action(final Object data) {
+                    dataArray[num] = data;
+                    boolean shouldFinish = false;
+                    synchronized (counter) {
+                        counter.count--;
+                        if (counter.count == 0) {
+                            shouldFinish = true;
+                        }
+                    }
+                    if (shouldFinish) {
+                        promise.finish(dataArray, true);
+                    }
+                    return null;
+                }
+            });
+        }
+        return promise;
+    }
+
+    private static class Counter {
+
+        public int count = 0;
+
+
+
+    }
+
     public static interface Action<Type, Return> {
         public Return action(final Type data);
     }
