f5662f1edb41f5d8fcef281e1ccae4ed4f9b875c
Semyon Danilov <samvimes@yandex.ru>
2014-11-12 03:06:42 +0300

    Update Main.java

--- src/com/danilov/Main.java
+++ src/com/danilov/Main.java
@@ -9,58 +9,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-//        final Promise<Integer> integerPromise = new Promise<Integer>();
-//
-//        Thread t1 = new Thread() {
-//            @Override
-//            public void run() {
-//                Integer a = 25;
-//                Integer b = 40;
-//                try {
-//                    Thread.sleep(5000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//                System.out.println("THREAD1");
-//                integerPromise.finish(a + b, true);
-//            }
-//        };
-//        final Promise<Double> doublePromise = integerPromise.then(new Promise.Action<Integer, Double>() {
-//            @Override
-//            public Double action(final Integer data) {
-//                return Double.valueOf("" + data) + 0.25d;
-//            }
-//        });
-//        Thread t2 = new Thread() {
-//            @Override
-//            public void run() {
-//                stringPromise = doublePromise.then(new Promise.Action<Double, String>() {
-//                    @Override
-//                    public String action(final Double data) {
-//                        return "" + data;
-//                    }
-//                });
-//                System.out.println("THREAD2");
-//                stringPromise.then(new Promise.Action<String, Void>() {
-//                    @Override
-//                    public Void action(final String data) {
-//                        finalPromise.finish(data, true);
-//                        return null;
-//                    }
-//                });
-//            }
-//        };
-//        t2.start();
-//        t1.start();
-//        finalPromise.then(new Promise.Action<String, Object>() {
-//            @Override
-//            public Object action(final String data) {
-//                System.out.println(data);
-//                return null;
-//            }
-//        });
-
         final Promise<Integer> p1 = new Promise<Integer>();
         Thread t1 = new Thread() {
             @Override
@@ -88,6 +36,62 @@ public class Main {
 
         t1.start();
         t2.start();
-
+        testMThreads();
     }
+    
+    private static void testMThreads() {
+
+        final Promise<Integer> integerPromise = new Promise<Integer>();
+
+        Thread t1 = new Thread() {
+            @Override
+            public void run() {
+                Integer a = 25;
+                Integer b = 40;
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("THREAD1");
+                integerPromise.finish(a + b, true);
+            }
+        };
+        final Promise<Double> doublePromise = integerPromise.then(new Promise.Action<Integer, Double>() {
+            @Override
+            public Double action(final Integer data) {
+                return Double.valueOf("" + data) + 0.25d;
+            }
+        });
+        Thread t2 = new Thread() {
+            @Override
+            public void run() {
+                stringPromise = doublePromise.then(new Promise.Action<Double, String>() {
+                    @Override
+                    public String action(final Double data) {
+                        return "" + data;
+                    }
+                });
+                System.out.println("THREAD2");
+                stringPromise.then(new Promise.Action<String, Void>() {
+                    @Override
+                    public Void action(final String data) {
+                        finalPromise.finish(data, true);
+                        return null;
+                    }
+                });
+            }
+        };
+        t2.start();
+        t1.start();
+        finalPromise.then(new Promise.Action<String, Object>() {
+            @Override
+            public Object action(final String data) {
+                System.out.println(data);
+                return null;
+            }
+        });
+        
+    }
+    
 }
