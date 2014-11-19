b5f89f140ad9d4fceab0c86d152bbb228d819c95
semyon <samvimes@yandex.ru>
2014-11-19 19:45:16 +0400

    asd

--- src/com/danilov/promise/Promise.java
+++ src/com/danilov/promise/Promise.java
@@ -1,40 +1,77 @@
 package com.danilov.promise;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Semyon on 10.11.2014.
  */
 public class Promise<Type> {
 
+    protected static ThreadPoolExecutor defaultExecutor = new ThreadPoolExecutor(10, 10,
+            0L, TimeUnit.MILLISECONDS,
+            new LinkedBlockingQueue<Runnable>());
+
     private Type data;
 
     private boolean isDone = false;
 
+    private boolean isSuccessful = false;
+
     private Action<Type, Void> onFinish = null;
 
+    private Action<Exception, Void> onCatch = null;
+
+    private boolean finishExecuted = false;
+
     public void finish(final Type data, final boolean success) {
+        boolean shouldExecute = false;
         synchronized (this) {
             isDone = true;
+            isSuccessful = success;
+            this.data = data;
+            if (onFinish != null && !finishExecuted) {
+                shouldExecute = true;
+                finishExecuted = true;
+            }
         }
-        this.data = data;
-        if (onFinish != null) {
-            onFinish.action(data);
+        if (shouldExecute) {
+            onFinish.action(data, success);
         }
+    }
+
+    //TODO: what if already executed and got exception?
+    public void exception(final Exception e) {
+        if (onCatch != null) {
+            onCatch.action(e, false);
+        }
+    }
+
+    public <X> Promise<X> catchException(final Action<Exception, X>) {
+
     }
 
     public <X> Promise<X> then(final Action<Type, X> action) {
         final Promise<X> nPromise = new Promise<X>();
-        this.onFinish = new Action<Type, Void>() {
-            @Override
-            public Void action(final Type data) {
-                X _data = action.action(data);
-                nPromise.finish(_data, true);
-                return null;
-            }
-        };
+        boolean shouldExecute = false;
         synchronized (this) {
-            if (isDone) {
-                this.onFinish.action(data);
+            this.onFinish = new Action<Type, Void>() {
+                @Override
+                public Void action(final Type data, final boolean success) {
+                    X _data = action.action(data, isSuccessful);
+                    nPromise.finish(_data, true);
+                    return null;
+                }
+            };
+            if (isDone && !finishExecuted) {
+                shouldExecute = true;
+                finishExecuted = true;
             }
+        }
+        if (shouldExecute) {
+            this.onFinish.action(data, isSuccessful);
         }
         return nPromise;
     }
@@ -55,7 +92,7 @@ public class Promise<Type> {
             final int num = i;
             promises[i].then(new Action() {
                 @Override
-                public Object action(final Object data) {
+                public Object action(final Object data, final boolean success) {
                     dataArray[num] = data;
                     boolean shouldFinish = false;
                     synchronized (counter) {
@@ -78,12 +115,26 @@ public class Promise<Type> {
 
         public int count = 0;
 
-
-
     }
 
     public static interface Action<Type, Return> {
-        public Return action(final Type data);
+        public Return action(final Type data, final boolean success);
+    }
+
+    public static <X> Promise<X> run(final Callable<X> callable) {
+        final Promise<X> promise = new Promise<X>();
+        defaultExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    X data = callable.call();
+                    promise.finish(data, true);
+                } catch (Exception e) {
+                    promise.exception(e);
+                }
+            }
+        });
+        return promise;
     }
 
 }
